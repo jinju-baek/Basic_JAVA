5a44037926bb7da94f351a90d8b44307560b76c8
jinju-baek <58206059+jinju-baek@users.noreply.github.com>
2019-12-12 16:44:07 +0900

    JAVA DB 연동

---
+++ src/jdbc/JdbcEx02.java
@@ -0,0 +1,36 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class JdbcEx02 {
+	public static void main(String[] args) {
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+		String user = "java";
+		String password = "1234";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // 정적으로 객체 생성
+			Connection conn = DriverManager.getConnection(url, user, password);
+			
+			String sql = "INSERT INTO tbl_study VALUES(?, ?, ?)";
+			PreparedStatement pstmt=conn.prepareStatement(sql); 
+			pstmt.setInt(1, 7); // (물음표 순서, 값)
+			pstmt.setString(2, "운이");
+			pstmt.setInt(3, 28);
+			
+			int result = pstmt.executeUpdate();
+			if(result > 0) {
+				System.out.println("등록성공");
+			}else {
+				System.out.println("등록실패");
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+		}
+	}
+}
